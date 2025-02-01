11ae6aaf6c8d2f467f4ba97c7374ed97e7a864c6
KunalBhika <kunalbhika2@gmail.com>
2025-02-02 02:02:39 +0530

    created scheduling.h custom header file with process class to hold data and algos definition

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# CPU_Scheduling
+# CPU Scheduling Algorithms
+
+This project implements various **CPU Scheduling Algorithms** in C++.  
+It includes a command-line interface (CLI) and will later have a **web-based visualization** using ReactJs and D3.js.
+
+## 📌 Algorithms Implemented
+- [ ] First Come First Serve (**FCFS**)
+- [ ] Shortest Job First (**SJF**)
+- [ ] Shortest Remaining Time First (**SRTF**)
+- [ ] Round Robin (**RR**)
+- [ ] Priority Scheduling
+- [ ] Multilevel Queue (**MLQ**)
---
+++ include/scheduling.h
@@ -0,0 +1,24 @@
+#ifndef SCHEDULING_H
+#define SCHEDULING_H
+
+#include<vector>
+
+class Process {
+    public:
+        int process_id;
+        int arrival_Time;
+        int burst_Time;
+        int priority;            // for priority scheduling algorithms
+        int completion_Time;
+        int turnaround_Time;
+        int wait_Time;
+};
+
+std::vector<Process> fcfs(std::vector<Process>);
+std::vector<Process> sjf(std::vector<Process>);
+std::vector<Process> srtf(std::vector<Process>);
+std::vector<Process> priorityScheduling(std::vector<Process> processes);
+std::vector<Process> rr(std::vector<Process>);
+std::vector<Process> mlq(std::vector<Process>);
+
+#endif
